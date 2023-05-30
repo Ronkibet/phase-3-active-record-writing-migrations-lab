a2606a05e64e953b07e8fedbb63d936aa71e3546
Ronkibet <ronniek625@gmail.com>
2023-05-30 09:53:58 +0300

    test done

--- db/migrate/01_create_students.rb
+++ db/migrate/01_create_students.rb
@@ -1,4 +1,10 @@
 class CreateStudents < ActiveRecord::Migration[6.1]
   def change
+    create_table :costumes do |t|
+      t.string :name
+      t.float :price
+      t.string :size
+      t.string :image_url
+      t.timestamps
   end
 end
